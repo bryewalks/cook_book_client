a1e32f8963fd74c11ab26d7adb937e25deee6229
bryewalks <bryewalks@gmail.com>
2019-02-08 09:52:01 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :client do
+    get '/recipes' => 'recipes#index'
+    get '/recipes/new' => 'recipes#new'
+    post '/recipes' => 'recipes#create'
+    get '/recipes/:id' => 'recipes#show'
+    get '/recipes/:id/edit' => 'recipes#edit'
+    patch '/recipes/:id' => 'recipes#update'
+  end
+end
